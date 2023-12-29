c69677042a6a86907ffe433287fefaf4d7a2d2f0
Vaniko Nozadze <ivane.nozadze.1@btu.edu.ge>
2023-12-29 23:17:49 +0400

    test path

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,7 @@
+import { BlogsService } from './../../services/blogs.service';
+import { Category } from './../../models/categorie.model';
+import { Blog } from './../../models/blog.model';
 import { Component, OnInit } from '@angular/core';
-import { Blog } from 'src/app/models/blog.model';
-import { Category } from 'src/app/models/categorie.model';
-import { BlogsService } from 'src/app/services/blogs.service';
 
 @Component({
   selector: 'app-home',
